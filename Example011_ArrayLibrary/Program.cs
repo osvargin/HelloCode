cdc4fadc551094db88d4b34c41a0f5862ab5b1af
osvargin <oleg_197@bk.ru>
2022-11-27 19:42:31 +0300

    add new code for Example 011

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -20,8 +20,32 @@ void PrintArray(int[] col)
         position++;
     }
 }
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
 
-int [] array = new int[10];
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        } 
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
